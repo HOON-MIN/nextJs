d2b45c1b46be13bd173ec5945fc2fa5c32edaeeb
HoonMin Kim <hunman90@naver.com>
2023-05-09 23:54:51 +0900

    5/9

--- pages/users/[name].jsx
+++ pages/users/[name].jsx
@@ -1,61 +1,8 @@
 import fetch from "isomorphic-unfetch";
-import styled from "styled-jsx/css";
-import { GoMail } from "react-icon/go";
-
-const style = styled`
-.profile-box{
-    width: 25%;
-    max-width : 272px;
-    margin-right :26px;
-    border : 1px solid #ababab;
-}
-.profile-image-wrapper {
-    width : 100%;
-    
-}
-.profile-image-wrapper .profile-image {
-    display : block;
-    width: 100%; 
-}
-
-.profile-username {
-    margin : 0 ,
-    padding-top : 16px; 
-    font-size : 26px; 
-}
-
-.profile-user-login {
-    margin : 0 ;
-    font-size : 20px;
-}
-
-.profile-user-bio {
-    margin : 0 ;
-    padding-top : 16px;
-    font-size : 14px;
-}
-`;
+import Profile from "./Profile";
 
 const name = ({ user }) => {
-  return (
-    <>
-      <div className="profile-box">
-        <div className="profile-image-wrapper">
-          <img
-            className="profile-image"
-            src={user.avatar_url}
-            alt={`${user.name} 프로필 이미지`}
-          />
-        </div>
-        <h2 className="profile-username">{user.name}</h2>
-        <p className="profile-user-login">{user.login}</p>
-        <p className="profile-user-bio">
-          {user.bio ? user.bio : "프로필 소개문 자리 입니다"}
-        </p>
-      </div>
-      <style jsx>{style}</style>
-    </>
-  );
+  return <Profile user={user} />;
 };
 
 name.getInitialProps = async ({ query }) => {
